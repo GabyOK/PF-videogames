c5e15e29ff0536fe5d512a365c821b4243f441af
Gaby <yesicasantos162@gmail.com>
2022-09-16 00:57:51 -0300

    agrego nueva ruta

--- api/src/db.js
+++ api/src/db.js
@@ -3,21 +3,19 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const {
-  DATABASE_URL,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
   //DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  //CONFIGURACION ADICIONAL
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  }
+);
 const basename = path.basename(__filename);
 const modelDefiners = [];
 
